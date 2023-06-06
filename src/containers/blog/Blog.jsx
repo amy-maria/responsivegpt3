09f9e9e0f9fd2a620f5b2db4db8f9ffa0ee65575
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-06-06 19:34:30 -0400

    blog and footer sections added

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,42 @@
 import React from "react";
 import "./blog.css";
+import { blog1, blog2, blog3, blog4, blog5 } from "./imports";
+import Article from "../../components/article/Article";
 
-const Blog = () => <div>Blog</div>;
+const Blog = () => (
+  <div className="gpt3__blog section-container">
+    <h1>A lot is happening, We are blogging about it.</h1>
+    <div className="gpt3__blog-content_container">
+      <div className="gpt3__blog-content_blog1">
+        <Article
+          imgUrl={blog1}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <div className="gpt3__blog-content_sub-container"></div>
+        <Article
+          imgUrl={blog2}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog3}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog4}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog5}
+          date="Sep 26, 2021"
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+      </div>
+    </div>
+  </div>
+);
 
 export default Blog;
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,6 +1,46 @@
 import React from "react";
 import "./footer.css";
+import logo from "../../assets/logo.svg";
 
-const Footer = () => <div>Footer</div>;
+const Footer = () => (
+  <div className="gpt3__footer section">
+    <div className="gpt3__footer-section-heading">
+      <h1>Do you want to step in to the future before others</h1>
+      <button>Request Early Access</button>
+    </div>
+    <div className="gpt3__footer-logo_container">
+      <img src={logo} alt="gpt_logo" />
+      <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+    </div>
+    <div className="gpt3__footer-links_container">
+      <h6>Links</h6>
+      <ul>
+        <li>Overons</li>
+        <li>Social Media</li>
+        <li>Counters</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+    <div className="gpt3__footer-company_container">
+      <h6>Company</h6>
+      <ul>
+        <li>Terms & Conditions</li>
+        <li>Privacy Policy</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+    <div className="gpt3__footer-contact_container">
+      <h6>Get In Touch</h6>
+      <ul>
+        <li>Crechterwoord K12 182 DK Alknjkcb</li>
+        <li>085-132567</li>
+        <li>info@payme.net</li>
+      </ul>
+      <div className="gpt3__footer-copyright">
+        <footer>© 2021 GPT-3. All rights reserved.</footer>
+      </div>
+    </div>
+  </div>
+);
 
 export default Footer;
