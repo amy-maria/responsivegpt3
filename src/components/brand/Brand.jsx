78128936a8749d81a220d7ea0efdd9728b772a04
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-06-05 23:46:46 -0400

    Added Brands section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+import "./brand.css";
 
-const Brand = () => <div>Brand</div>;
-
+const Brand = () => (
+  <div className="gpt3__brand-icons">
+    <div>
+      <img src={google} alt="google-icon" />
+    </div>
+    <div>
+      <img src={slack} alt="slack-icon" />
+    </div>
+    <div>
+      <img src={atlassian} alt="atlassian-icon" />
+    </div>
+    <div>
+      <img src={dropbox} alt="dropbox-icon" />
+    </div>
+    <div>
+      <img src={shopify} alt="shopify-icon" />
+    </div>
+  </div>
+);
 export default Brand;
