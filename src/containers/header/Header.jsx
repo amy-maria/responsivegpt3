94eda2ce367a42014803cea9afb7a5d6e73a926f
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-06-05 23:28:38 -0400

    Header section completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">
-        Let&apos;s Build Something amazing with GPT-3 OpenAI
+        Let's Build Something amazing with GPT-3 OpenAI
       </h1>
       <p>
         Yet bed any for travelling assistance indulgence unpleasing. Not
