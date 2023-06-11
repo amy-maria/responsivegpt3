333ebe089a0f19d9d6daf7c2a59ca3aefc68971a
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-06-11 16:59:56 -0400

    Corrected CTA css

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -6,9 +6,9 @@ const Cta = () => (
     <div className="gpt3__cta-section">
       <h5>Request Early Access to Get Started</h5>
       <h1>Register today & start exploring the endless possiblities.</h1>
-      <div className="gpt3__cta-btn">
-        <button>Get Started</button>
-      </div>
+    </div>
+    <div className="gpt3__cta-btn">
+      <button>Get Started</button>
     </div>
   </div>
 );
