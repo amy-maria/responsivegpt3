e94e73614cb60fe73b4654e346387c1b36f19a5d
amy-maria <122695916+amy-maria@users.noreply.github.com>
2023-06-11 22:55:11 -0400

    Completed blog and footer styling

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -8,36 +8,39 @@ const Footer = () => (
       <h1>Do you want to step in to the future before others</h1>
       <button>Request Early Access</button>
     </div>
-    <div className="gpt3__footer-logo_container">
-      <img src={logo} alt="gpt_logo" />
-      <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
-    </div>
-    <div className="gpt3__footer-links_container">
-      <h6>Links</h6>
-      <ul>
-        <li>Overons</li>
-        <li>Social Media</li>
-        <li>Counters</li>
-        <li>Contact</li>
-      </ul>
-    </div>
-    <div className="gpt3__footer-company_container">
-      <h6>Company</h6>
-      <ul>
-        <li>Terms & Conditions</li>
-        <li>Privacy Policy</li>
-        <li>Contact</li>
-      </ul>
-    </div>
-    <div className="gpt3__footer-contact_container">
-      <h6>Get In Touch</h6>
-      <ul>
-        <li>Crechterwoord K12 182 DK Alknjkcb</li>
-        <li>085-132567</li>
-        <li>info@payme.net</li>
-      </ul>
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-logo_container">
+        <img src={logo} alt="gpt_logo" />
+        <p>
+          Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+        </p>
+      </div>
+      <div className="gpt3__footer-links_container">
+        <h6>Links</h6>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-company_container">
+        <h6>Company</h6>
+
+        <p>Terms & Conditions</p>
+        <p>Privacy Popcy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-contact_container">
+        <h6>Get In Touch</h6>
+
+        <p>
+          Crechterwoord K12 <br />
+          182 DK Alknjkcb
+        </p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+      </div>
       <div className="gpt3__footer-copyright">
-        <footer>© 2021 GPT-3. All rights reserved.</footer>
+        <p>© 2021 GPT-3. All rights reserved.</p>
       </div>
     </div>
   </div>
